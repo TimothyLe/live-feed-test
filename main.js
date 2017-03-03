1c4d5c8fdb815d4fe0f1199a082afbb1754694a8
timothyle <timothyle85@gmail.com>
2017-03-03 15:22:20 -0800

    Updating files

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
-var aReq = new XMLHttpRequest();
-aReq.open('GET', 'https://github.com/TimothyLe/live-feed-test/blob/master/json/feed-1.json');
-aReq.onload = function() {
+/*$.ajax({
+    type: 'GET',
+    dataType: 'jsonp',
+    url: 'https://github.com/TimothyLe/live-feed-test/blob/master/json/feed-1.json',
+    crossDomain : true,
+    xhrFields: {
+        withCredentials: true
+    },
+	success: function(aData) {
+		console.log("It worked!", aData);
+	}
+});*/
+var containerRefresh = document.getElementById("feed-info");
+var btn = document.getElementById("btn"); //runs when button pressed
+	btn.addEventListener('click', function(){
+	var aReq = new XMLHttpRequest(); //runs when webpage opens
+	aReq.open('GET', 'https://github.com/TimothyLe/live-feed-test/blob/master/json/feed-1.json');
+	aReq.onload = function() {
 	var aData = JSON.parse(aReq.ResponseText());
-	console.log(aData[0]);
+	renderHTML(aData);
+}
+aReq.send();
+});
+
+function renderHTML(data){
+	containerRefresh.insertAdjacentHTML('beforeend', 'testing123');
 }
 
-aReq.send();
+
+
