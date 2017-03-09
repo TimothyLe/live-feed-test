b01dc03ddbac97de339e110a8fb7d2ac5900c118
Thinh Le <timothyle85@gmail.com>
2017-03-08 22:34:57 -0800

    Update the main js file
    New functions and definitions

--- main.js
+++ main.js
@@ -11,27 +11,46 @@
 	}
 });*/
 
+
 // CORS
 // Access-Control-Allow-Origin
 // Access-Control-Allow-Method
 // Access-Control-Allow-Header
 // next()
 
+var page = 1;
 var containerRefresh = document.getElementById("feed-info");
-var btn = document.getElementById("btn"); //runs when button pressed
+var btn = document.getElementById("btn"); 
+btn.use(function(req, res, next){
+	// Creates header for access control
+	res.header('Access-Control-Allow-Origin', "*");
+	next();
+})
+// runs when button pressed
 	btn.addEventListener('click', function(){
-	var aReq = new XMLHttpRequest(); //runs when webpage opens
-	aReq.open('GET', 'https://github.com/TimothyLe/live-feed-test/blob/master/json/feed-1.json');
+		// Collects data on click of the button
+	var aReq = new XMLHttpRequest(); 
+	// runs when webpage opens
+	aReq.open('GET', 'https://github.com/TimothyLe/live-feed-test/blob/master/json/feed-'+page+'.json');
 	aReq.onload = function() {
-	var aData = JSON.parse(aReq.ResponseText());
-	renderHTML(aData);
+	var data = JSON.parse(aReq.ResponseText());
+	renderHTML(data);
 }
 aReq.send();
+page++;
 });
 
 function renderHTML(data){
-	containerRefresh.insertAdjacentHTML('beforeend', 'testing123');
+	// Prints out data from JSON files
+	var htmlStr = "";
+	for(i = 0; i < data.length; i++){
+	htmlStr += "<p>" + "\"" + data[i].message + "\"" + " sent by " + data[i].sender + " " + data[i].date + " ago.</p>";
 }
+	containerRefresh.insertAdjacentHTML('beforeend', htmlStr);
+}
+
+
+
 
 
 
