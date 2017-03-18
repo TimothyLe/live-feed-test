44e0bc3f339c668fc21caa5c1d451577ca90b3c3
Thinh Le <timothyle85@gmail.com>
2017-03-18 03:42:03 -0700

    Linking to oAuth
    Linking app to oAuth for access to Twitter API

--- main.js
+++ main.js
@@ -18,6 +18,12 @@
 // Access-Control-Allow-Header
 // next()
 
+// Replace() clears window session history which keeps the original page out of the loop
+window.location.replace("https://oauth2server.com/auth?response_type=code&client_id=CLIENT_ID&redirect_uri=REDIRECT_URI&scope=photos&state=1234zyx");
+
+// Href is similar but latches onto the original page the user was on
+// window.location.href = "...";
+
 var page = 1;
 var containerRefresh = document.getElementById("feed-info");
 var btn = document.getElementById("btn"); 
