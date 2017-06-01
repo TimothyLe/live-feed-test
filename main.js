38dee76d343bdc28d8ab6cf0cb54c9d6e9d01bcf
timothyle <timothyle85@gmail.com>
2017-05-31 22:27:25 -0700

    Modified json

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-/*$.ajax({
+/* Request Key jQuery
+$.ajax({
     type: 'GET',
     dataType: 'jsonp',
     url: 'https://github.com/TimothyLe/live-feed-test/blob/master/json/feed-1.json',
@@ -10,20 +11,33 @@
 		console.log("It worked!", aData);
 	}
 });*/
+
+// Replace() clears window session history which keeps the original page out of the loop
+window.location.replace("https://oauth2server.com/auth?response_type=code&client_id=CLIENT_ID&redirect_uri=REDIRECT_URI&scope=photos&state=1234zyx");
+
+// Href is similar but latches onto the original page the user was on
+// window.location.href = "...";
+
+var dataSet = 1;
 var containerRefresh = document.getElementById("feed-info");
 var btn = document.getElementById("btn"); //runs when button pressed
 	btn.addEventListener('click', function(){
 	var aReq = new XMLHttpRequest(); //runs when webpage opens
-	aReq.open('GET', 'https://github.com/TimothyLe/live-feed-test/blob/master/json/feed-1.json');
+	aReq.open('GET', 'https://github.com/TimothyLe/live-feed-test/blob/master/json/feed-' + dataSet + '.json');
 	aReq.onload = function() {
-	var aData = JSON.parse(aReq.ResponseText());
+	var aData = JSON.parse(aReq.open());
 	renderHTML(aData);
 }
 aReq.send();
-});
+dataSet++;
+}, false);
 
-function renderHTML(data){
-	containerRefresh.insertAdjacentHTML('beforeend', 'testing123');
+function renderHTML(aData){
+	var htmlStr = "";
+	for(i = 0; i <aData.length; i++){
+		htmlStr += "<p>" + aData[i].message + " sent by " + aData[i].sender + " " + aData[i].date + " ago." + "</p>";
+	}
+	containerRefresh.insertAdjacentHTML('beforeend', htmlStr);
 }
 
 
