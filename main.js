99e4a49bdd982c9ce6182083cca1658db476ff07
timothyle <timothyle85@gmail.com>
2017-06-29 01:49:09 -0700

    React native example

--- main.js
+++ main.js
@@ -75,3 +75,20 @@ if (!xhr) {
   throw new Error('CORS not supported');
 }
 */
+
+/*!< React Native - Hello World Example*/
+/*
+import React, { Component } from 'react';
+import { AppRegistry, Text } from 'react-native';
+
+export default class HelloWorldApp extends Component {
+  render() {
+    return (
+      <Text>Hello world!</Text>
+    );
+  }
+}
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('HelloWorldApp', () => HelloWorldApp);
+*/
